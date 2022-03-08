b4c8800ce341152e2913ee4966356e32afd67877
ruanchaves <ruanchaves93@gmail.com>
2022-03-08 09:44:23 +0100

    fix argparse no_comment no_submission

--- reddit_download.py
+++ reddit_download.py
@@ -28,8 +28,8 @@ def get_args():
     parser.add_argument('--end_year', action='store', type=int, default=2021)
     parser.add_argument('--start_month', action='store', type=int, default=1)
     parser.add_argument('--end_month', action='store', type=int, default=12)
-    parser.add_argument('-s', '--submission', action='store_true', default=True)
-    parser.add_argument('-c', '--comment', action='store_true', default=True)
+    parser.add_argument('-ns', '--no_submission', action='store_true')
+    parser.add_argument('-nc', '--no_comment', action='store_true')
     parser.add_argument('--submission_dir', action='store', type=str, default="./submission")
     parser.add_argument('--comment_dir', action='store', type=str, default="./comment")
     parser.add_argument('--extensions', action='store', type=str, default="zst,bz2")
@@ -100,6 +100,8 @@ def download_dumps(
 def main():
     args = get_args()
 
+    logger.debug(args)
+
     pathlib.Path(args.submission_dir).mkdir(parents=True, exist_ok=True)
     pathlib.Path(args.comment_dir).mkdir(parents=True, exist_ok=True)
     
@@ -110,8 +112,8 @@ def main():
         args.end_year, 
         args.start_month, 
         args.end_month, 
-        submission=args.submission, 
-        comment=args.comment, 
+        submission=not args.no_submission, 
+        comment=not args.no_comment, 
         extensions=extensions,
         submission_dir=args.submission_dir,
         comment_dir=args.comment_dir)
